dff71edee09cf0747e98933a1225cf47a0361ea7
Chandra Ghalley <02230057.cst.edu.bt>
2024-03-01 15:56:39 +0530

    First commit

---
+++ logical.py
@@ -0,0 +1,38 @@
+#Example 1
+x = 5
+print( x < 3 and x > 10) # False because both conditions are false
+ 
+# Example 2
+y = 12 
+print(y < 10 or y % 2 == 0 ) # True because the second condition is true 
+
+# Example 3
+a = 5
+print(not(a > 3 and a < 10)) # False cause the condition inside is not is "Ture"
+
+# Example 4
+b = 12
+print(not(b >10 and b % 5 == 0))
+
+# Example 5
+q = 20
+print(q == 3 and q < 23)
+
+# EX 6
+w = 10
+print(w > 4 or w < 20)
+
+# Ex 7
+r = 14
+print(r > 20 or r < 12)
+
+# Ex 8
+r = 14
+print(r > 20 or r < 12)
+
+# EX 9
+r = 14
+print(not(r > 20 or r < 12))
+
+
+
