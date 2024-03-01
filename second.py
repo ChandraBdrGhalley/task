c9e9eae8d5b9ed6c1ff6959edc17991678b2599e
Chandra Ghalley <02230057.cst.edu.bt>
2024-03-01 14:43:13 +0530

    first commit

---
+++ second.py
@@ -0,0 +1,9 @@
+#a = input("Enter your name: ")
+#num1 = int(input("Enter the value: "))
+#num2 = int(input("Enter the value: "))
+#print(num1 * num2)
+
+a = str(input("Enter your name: "))
+num1 = int(input("Enter the value: "))
+num2 = int(input("Enter the value: "))
+print(num1 * num2)
